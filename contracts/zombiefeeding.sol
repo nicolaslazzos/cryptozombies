3b699b21615952e693d99afd2b374cd32ab30ef3
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-08-25 21:04:55 -0300

    adding missing function param

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -22,7 +22,7 @@ contract ZombieFeeding is ZombieFactory {
   address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
   KittyInterface kittyContract = KittyInterface(ckAddress);
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
