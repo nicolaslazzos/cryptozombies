c904a226bcd2183b0fc450f01493362ab5a1d6ff
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-08-29 22:20:08 -0300

    adding safemath openzeppelin library to avoid overflows

--- contracts/zombiefactory.sol
+++ contracts/zombiefactory.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.4.19;
 
 import "./ownable.sol";
+import "./safemath.sol";
 
 contract ZombieFactory is Ownable {
+    
+    using SafeMath for uint256;
 
     event NewZombie(uint zombieId, string name, uint dna);
 
