16f96cdfc4c15f4863e7ce86f8fb0d46e9422e67
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-09-27 19:06:00 -0300

    adding selfdestruct function and redeploying contract

--- src/ethereum/contracts/CryptoZombies.sol
+++ src/ethereum/contracts/CryptoZombies.sol
@@ -2,4 +2,8 @@ pragma solidity ^0.4.25;
 
 import "./zombieownership.sol";
 
-contract CryptoZombies is ZombieOwnership {}
+contract CryptoZombies is ZombieOwnership {
+  function kill() public onlyOwner {
+    selfdestruct(msg.sender);
+  }
+}
